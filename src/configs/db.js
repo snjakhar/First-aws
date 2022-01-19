f07a5fffe0f0c697409136f87776e79422d493d7
Srinivas Jakhar <snjakhar.cse@gmail.com>
2022-01-19 22:50:32 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/web13-ejs");
+  return mongoose.connect("mongodb+srv://snjakhar:Myproject@86>@cluster0.inizv.mongodb.net/testingAtlas");
 };
